2331d402390b320360538ce2efc08117e2ba3687
sanchitaghosh2710-byte <sanchitaghosh2710@gmail.com>
2026-02-08 12:28:20 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,55 @@
+/*
+let score = 33
+console.log(typeof score)
+console.log(typeof (score))
+*/
+
+/*
+let scores = "33abc"
+console.log(typeof scores)
+
+
+let valueInNumber = Number(scores) // value to number
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let scores = null
+let valueInNumber = Number(scores) // value to number
+console.log(typeof valueInNumber) // number
+console.log(valueInNumber)  // 0
+
+
+let scores = undefined
+let valueInNumber = Number(scores) // value to number
+console.log(typeof valueInNumber)
+console.log(valueInNumber)  // NaN
+
+
+let scores = true
+let valueInNumber = Number(scores) // value to number
+console.log(typeof valueInNumber)
+console.log(valueInNumber)   /// 1
+
+
+
+let scores= "hitesh"
+let valueInNumber = Number(scores) // value to number
+console.log(typeof valueInNumber)
+console.log(valueInNumber)  //NaN
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// "hitesh" => NaN
+// "true" =>1 , false => 0
+
+let loggedIn = 1
+let loggedInIsboolean = Boolean(loggedIn)
+console.log(loggedInIsboolean)
+*/
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
