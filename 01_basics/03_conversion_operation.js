f344b28d3251e222ebe802bee3b35490ed702fe8
sanchitaghosh2710-byte <sanchitaghosh2710@gmail.com>
2026-02-08 17:30:41 +0000

    conversion

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -47,9 +47,70 @@ console.log(valueInNumber)  //NaN
 let loggedIn = 1
 let loggedInIsboolean = Boolean(loggedIn)
 console.log(loggedInIsboolean)
-*/
+
 
 let someNumber=33
 let stringNumber= String(someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber)
+
+*/
+
+
+//******************************** Operations(String to Number) // ******************************
+
+
+/*
+let value = 3
+let negValue = -value
+console.log(negValue)  // => -3
+*/
+
+/*
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1 + str2
+console.log(str3)    // helloworld
+*/
+
+/*
+console.log("1" + 2)   // => 12
+console.log(1 + "2")   // =>12
+console.log("1" + 2 + 2)  // => 122
+console.log(1 + 2 + "2")  // =>32
+
+
+
+console.log(true)  // => true
+console.log(+true)  // => 1
+// console.log(true+)  => error
+
+console.log(+" ")   // => 0
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)   // => 101
+*/
+
+
+// *************** Comparison of Datatypes ********************** //
+
+
+/*
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+*/
+/*
+console.log("2" > 1)  // => true
+console.log("02" > 1)  // => true
+
+console.log(null > 0)  // => false
+console.log(null == 0)   // => false
+console.log(null >= 0)   // => true
+*/
+// same for undefined 
+console.log("2" === 2)   // checks the datatype first before conversion
