f174446db7c961af507c8ad339cab7418b55d745
sanchitaghosh2710-byte <sanchitaghosh2710@gmail.com>
2026-02-08 10:29:47 +0000

    variable & datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "sanchita@gmail.com"
+var accountPassword = "223456"
+accountCity = "Jaipur"
+console.log(accountId);
+
+// accountId = 2 // Not allowded
+
+accountEmail = "ghoshsanchita@gmail.com"
+accountPassword = "567321"
+accountCity = "Kolkata"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // traet all js code as newer version
+// alert(3 + 3) // we are using node.js not browser
+
+console.log(3 + 3) 
+
+let name="sanchita"
+let age=21
+let loggedIn=false
+
+
+// Primitive datatypes //
+ // number = 2 to power 52
+ // bigint
+ // string = " " / ''
+ // boolean = true / false
+ // null = standalone value / representation of empty value
+ // undefined = value which is not assigned till now
+ // symbol = unique
+
+// OBJECT //
+ console.log(typeof "sanchita");
+
+ console.log(typeof null)   // o/p will be object
+ 
+ console.log(typeof undefined)  // o/p is undefined
