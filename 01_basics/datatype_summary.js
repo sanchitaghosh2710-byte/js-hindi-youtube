e6b29ab661cfeba6f1685cb3c058984732970def
sanchitaghosh2710-byte <sanchitaghosh2710@gmail.com>
2026-02-09 03:15:56 +0000

    summary of datatype

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,50 @@
+// ** Depending on how data are stored and accessed from memory , there are two types of  data
+
+// Primitive datatype(7 types)
+    // Number -> Number
+    // string -> string
+    // Boolean -> boolean
+    // Null -> object
+    // Undefined -> undefined
+    // synbol -> symbol
+    // bigInt -> bigInt
+
+
+
+
+// Non-Primitive  or Reference
+    // Arrays -> object
+    // Object -> Object
+    // Function -> Function
+
+/*
+const score = 100
+const scoreValue = 100.3
+const outsideTemp = null
+let userEmail;
+const isLoggedIn = false
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+
+const bigInt= 12345678923456732490813n
+console.log(typeof bigInt)
+*/
+
+// **************************************************************
+
+/*
+ const Arrays=["Shaktiman", "Naagraj", "Doga"]
+
+let myObj={
+            myName: "Hitesh",
+            age: 22 }
+
+let myFunction= {
+                    console.log("Hello World");
+                }
+
+*/
